a770c0e0e4e6d6afd86768fca2f9fa0e64917cc4
Ajayydev <136185490+Ajibuah@users.noreply.github.com>
2023-09-14 00:47:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+a= int(input("Enter the first number: "))
+b = int(input("Enter the Second number: "))
+l=[]
+for i in range(a):
+    l.append(1)
+for i in range(b):
+    l.append(1)
+print(f"sum of {a} and {b} : {len(l)}")
