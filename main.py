950d299706cd8507670f414ea85170d19273fda9
Ajayydev <136185490+Ajibuah@users.noreply.github.com>
2023-09-14 10:56:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ for i in range(a):
 for i in range(b):
     l.append(1)
 print(f"sum of {a} and {b} : {len(l)}")
+           or
+def addition (a,b):
+    print(f"the sum of {a} and {b} is" , sum({a,b}))
+addition(20,30)
+
